4d73c9a88a94a749d02e97c46a468435e4872f44
logonrm <logonrm@fiap.com.br>
2018-04-09 11:30:32 -0300

    aula que o professor faltou

---
+++ src/ViewCliente.java
@@ -0,0 +1,123 @@
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+
+public class ViewCliente extends JFrame implements ActionListener {
+
+private JLabel lblId;
+private JLabel lblNome;
+private JLabel lblLogradouro;
+private JLabel lblNumero;
+private JLabel lblComplemento;
+private JLabel lblCep;
+private JLabel lblef;
+private JTextField txtId;
+private JTextField txtNome;
+private JTextField txtLogradouro;
+private JTextField txtNumero;
+private JTextField txtComplemento;
+private JTextField txtCep;
+private JTextField txtCpf;
+private JButton btnOk;
+
+public ViewCliente() {
+	super("Clientes");
+	setLayout(new FlowLayout());
+	
+	lblId = new JLabel("Id:");
+	add(lblId);
+	
+	txtId = new JTextField(10);
+	add(txtId);
+	
+	lblNome = new JLabel("Nome:");
+	add(lblNome);
+	
+	txtNome = new JTextField(10);
+	add(txtNome);
+	
+	lblLogradouro = new JLabel("Rua:");
+	add(lblLogradouro);
+	
+	txtLogradouro = new JTextField(10);
+	add(txtLogradouro);
+	
+	lblNumero = new JLabel("Nº:");
+	add(lblNumero);
+	
+	txtNumero = new JTextField(10);
+	add(txtNumero);
+	
+	lblComplemento = new JLabel("Complemento:");
+	add(lblComplemento);
+	
+	txtComplemento = new JTextField(10);
+	add(txtComplemento);
+	
+	lblCep = new JLabel("CEP:");
+	add(lblCep);
+	
+	txtCep = new JTextField(10);
+	add(txtCep);
+	
+	lblef = new JLabel("CPF:");
+	add(lblef);
+	
+	txtCpf=new JTextField(10);
+	add(txtCpf);
+	
+	btnOk = new JButton("Cadastrar");
+	add(btnOk);
+	
+	btnOk.addActionListener(this);
+	
+	
+	}
+
+
+public void actionPerformed(ActionEvent event) {
+	if(event.getSource() == btnOk) {
+		
+		if (txtId.getText().isEmpty()){
+		    JOptionPane.showMessageDialog(null, "Vc esqueceu o campo " + "ID" + " em branco");
+		}  
+		if (txtNome.getText().isEmpty()) {
+		    JOptionPane.showMessageDialog(null, "Vc esqueceu o campo " + "Nome" + " em branco");
+		}
+		if (txtLogradouro.getText().isEmpty()) {
+		    JOptionPane.showMessageDialog(null, "Vc esqueceu o campo " + "Endereco" + " em branco");
+		}
+		if (txtNumero.getText().isEmpty()) {
+		    JOptionPane.showMessageDialog(null, "Vc esqueceu o campo " + "Numero" + " em branco");
+		}
+		if (txtComplemento.getText().isEmpty())  {
+		    JOptionPane.showMessageDialog(null, "Vc esqueceu o campo " + "Complemento" + " em branco");
+		}
+		if (txtCep.getText().isEmpty())  {
+		    JOptionPane.showMessageDialog(null, "Vc esqueceu o campo " + "CEP" + " em branco");
+		}
+		if (txtCpf.getText().isEmpty()){
+		    JOptionPane.showMessageDialog(null, "Vc esqueceu o campo " + "CPF" + " em branco");
+		}else {
+			JOptionPane.showMessageDialog(null, "Cadastrado com Sucesso");
+		}
+
+	}
+}
+
+public static void main(String[] args) {
+	ViewCliente janelinha = new ViewCliente();
+	janelinha.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	janelinha.setSize(100,600);
+	janelinha.setVisible(true);
+			
+}
+
+}
+
